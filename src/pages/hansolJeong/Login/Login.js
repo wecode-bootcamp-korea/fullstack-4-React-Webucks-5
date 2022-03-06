924c2889cb2df7464cc936204872b88be55d1a25
HANSOL JEONG <losnah24@gmail.com>
2022-03-07 00:33:21 +0900

    Modify: scss 추가

--- src/pages/hansolJeong/Login/Login.js
+++ src/pages/hansolJeong/Login/Login.js
@@ -3,11 +3,10 @@ import { Link, useNavigate } from 'react-router-dom';
 import React from 'react';
 
 function Login() {
-
   const navigate = useNavigate();
 
   function goToList() {
-    navigate('/list');
+    navigate('/list-hansol');
   }
 
   return (
@@ -15,16 +14,24 @@ function Login() {
       <section className="login-container">
         <div className="logo">Webucks</div>
         <div className="loginForm" action="./list.html">
-            <input className="id" type="text" placeholder="전화번호, 사용자 이름 또는 이메일"/>
-            <input className="pw" type="password" placeholder="비밀번호"/>
-            <button className="login-btn" 
-              onClick={() => {
-                goToList();
-            }}>로그인</button>
-            {/* <div className="forgot-pw">비밀번호를 잊으셨나요?</div> */}
-            <Link to="/signup">비밀번호를 잊으셨나요?</Link>
+          <input
+            className="id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input className="pw" type="password" placeholder="비밀번호" />
+          <button
+            className="login-btn"
+            onClick={() => {
+              goToList();
+            }}
+          >
+            로그인
+          </button>
+          {/* <div className='forgot-pw'>비밀번호를 잊으셨나요?</div> */}
+          <Link to="/signup">비밀번호를 잊으셨나요?</Link>
         </div>
-    </section>
+      </section>
     </div>
   );
 }
